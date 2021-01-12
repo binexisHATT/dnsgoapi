b8bd3cfece2ab5280c6e19d0dcd1f6b339fa4bf0
Alexis Rodriguez <rodriguez10011999@gmail.com>
2021-01-12 15:22:43 -0500

    Updating dnsgoapi/lookup.go

--- cmd/dnsgoapi/main.go
+++ cmd/dnsgoapi/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-    "flag"
-    "os"
-    "io/ioutil"
+	"os"
 
 	"github.com/gorilla/mux"
 	"github.com/russross/blackfriday"
@@ -15,25 +15,25 @@ import (
 )
 
 func main() {
-    port := flag.Int("port", 8080, "The port for the server to listen on")
-    flag.Parse()
+	port := flag.Int("port", 8080, "The port for the server to listen on")
+	flag.Parse()
 
 	r := mux.NewRouter()
-    f, err := os.Open("./README.md")
-    if err != nil {
-        log.Fatal("Unable to open README.md file")
-    }
-    defer f.Close()
-    markdown, err := ioutil.ReadAll(f)
+	f, err := os.Open("./README.md")
+	if err != nil {
+		log.Fatal("Unable to open README.md file")
+	}
+	defer f.Close()
+	markdown, err := ioutil.ReadAll(f)
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, string(blackfriday.MarkdownCommon(markdown)))
+		fmt.Fprintf(w, string(blackfriday.MarkdownCommon(markdown)))
 	}).Methods("GET")
 
 	r.HandleFunc("/a/{PublicDNS}/{q}", dnsgoapi.QueryA).Methods("GET")
 	r.HandleFunc("/aaaa/{PublicDNS}/{q}", dnsgoapi.QueryQuadA).Methods("GET")
 	r.HandleFunc("/cname/{PublicDNS}/{q}", dnsgoapi.QueryCNAME).Methods("GET")
 
-    l := fmt.Sprintf(":%d", *port)
-    log.Printf("Listening on %s", l)
-    log.Fatal(http.ListenAndServe(l,  r))
+	l := fmt.Sprintf(":%d", *port)
+	log.Printf("Listening on %s", l)
+	log.Fatal(http.ListenAndServe(l, r))
 }
